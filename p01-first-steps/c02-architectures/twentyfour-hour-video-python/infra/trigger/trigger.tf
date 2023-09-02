4ac3d743223eba15b9f91e466fe8b241a7463217
wilsonify <tom.a.wilson@gmail.com>
2023-09-02 16:53:29 -0400

    unique name

--- p01-first-steps/c02-architectures/twentyfour-hour-video-python/infra/trigger/trigger.tf
+++ p01-first-steps/c02-architectures/twentyfour-hour-video-python/infra/trigger/trigger.tf
@@ -8,7 +8,7 @@ resource "aws_s3_bucket_notification" "s3_trigger" {
   }
 }
 resource "aws_lambda_permission" "aws_lambda_s3_trigger_permission" {
-  statement_id  = "AllowS3Invoke"
+  statement_id  = "AllowS3UploadToInvoketwentyfourHourVideoPython"
   action        = "lambda:InvokeFunction"
   function_name = "function:twentyfour-hour-video-python-transcode-video"
   principal     = "s3.amazonaws.com"
