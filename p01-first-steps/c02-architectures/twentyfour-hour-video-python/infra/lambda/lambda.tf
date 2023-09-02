23438984979e3091437c36d05e18b02428726263
wilsonify <tom.a.wilson@gmail.com>
2023-09-02 16:46:18 -0400

    move trigger

--- p01-first-steps/c02-architectures/twentyfour-hour-video-python/infra/lambda/lambda.tf
+++ p01-first-steps/c02-architectures/twentyfour-hour-video-python/infra/lambda/lambda.tf
@@ -25,12 +25,3 @@ resource "aws_lambda_function" "twentyfour-hour-video-python-transcode-video" {
 
 }
 
-resource "aws_s3_bucket_notification" "s3_trigger" {
-  bucket = aws_s3_bucket.s3-serverless-video-upload-python.id  # Replace with your S3 bucket ID
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.twentyfour-hour-video-python-transcode-video.arn
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "uploads/"
-    filter_suffix       = ".mp4"
-  }
-}
--- p01-first-steps/c02-architectures/twentyfour-hour-video-python/infra/trigger/trigger.tf
+++ p01-first-steps/c02-architectures/twentyfour-hour-video-python/infra/trigger/trigger.tf
@@ -1,9 +1,16 @@
 resource "aws_s3_bucket_notification" "s3_trigger" {
-  bucket = "064592191516-serverless-video-upload-python"  # Replace with your S3 bucket ID
+  bucket = "064592191516-serverless-video-upload-python"
   lambda_function {
     lambda_function_arn = "arn:aws:lambda:us-east-1:064592191516:function:twentyfour-hour-video-python-transcode-video"
     events              = ["s3:ObjectCreated:*"]
     filter_prefix       = "uploads/"
     filter_suffix       = ".mp4"
   }
+}
+resource "aws_lambda_permission" "aws_lambda_s3_trigger_permission" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "function:twentyfour-hour-video-python-transcode-video"
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::064592191516-serverless-video-upload-python"
 }
