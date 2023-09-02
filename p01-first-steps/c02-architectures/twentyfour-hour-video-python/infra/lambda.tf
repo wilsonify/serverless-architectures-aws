239f1791438781b7a8faa16a2dc3c005ff93651e
wilsonify <tom.a.wilson@gmail.com>
2023-09-02 14:30:48 -0400

    add bucket notification to lambda

--- p01-first-steps/c02-architectures/twentyfour-hour-video-python/infra/lambda.tf
+++ p01-first-steps/c02-architectures/twentyfour-hour-video-python/infra/lambda.tf
@@ -1,6 +1,12 @@
 resource "aws_lambda_function" "twentyfour-hour-video-python-transcode-video" {
-  architectures = ["x86_64"]
-
+  function_name                  = "twentyfour-hour-video-python-transcode-video"
+  architectures                  = ["x86_64"]
+  image_uri                      = "064592191516.dkr.ecr.us-east-1.amazonaws.com/transcode_video_python:latest"
+  memory_size                    = "1024"
+  package_type                   = "Image"
+  reserved_concurrent_executions = "-1"
+  role                           = "arn:aws:iam::064592191516:role/transcode-video"
+  source_code_hash               = "34e2a1603ba3bceb7f0e51ccdef6d5a1c90c4d68af2bc3bf6af4491a13e8d688"
   environment {
     variables = {
       MEDIA_ENDPOINT          = "https://lxlxpswfb.mediaconvert.us-east-1.amazonaws.com"
@@ -8,26 +14,23 @@ resource "aws_lambda_function" "twentyfour-hour-video-python-transcode-video" {
       TRANSCODED_VIDEO_BUCKET = "064592191516-serverless-video-transcode-python"
     }
   }
-
-  function_name = "twentyfour-hour-video-python-transcode-video"
-
-  image_config {
-    command = ["transcode_video_python/__main__.lambda_handler"]
-  }
-
-  image_uri                      = "064592191516.dkr.ecr.us-east-1.amazonaws.com/transcode_video_python:latest"
-  memory_size                    = "1024"
-  package_type                   = "Image"
-  reserved_concurrent_executions = "-1"
-  role                           = "arn:aws:iam::064592191516:role/transcode-video"
-  source_code_hash               = "34e2a1603ba3bceb7f0e51ccdef6d5a1c90c4d68af2bc3bf6af4491a13e8d688"
-
+  image_config { command = ["transcode_video_python/__main__.lambda_handler"] }
   tags = {
     STAGE = "dev"
   }
   timeout = "6"
-
   tracing_config {
     mode = "PassThrough"
+  }
+
+}
+
+resource "aws_s3_bucket_notification" "s3_trigger" {
+  bucket = aws_s3_bucket.s3-serverless-video-upload-python.id  # Replace with your S3 bucket ID
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.twentyfour-hour-video-python-transcode-video.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "uploads/"
+    filter_suffix       = ".mp4"
   }
 }
