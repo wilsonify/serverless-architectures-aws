93f962057194071165edf083760dc2d4301874ad
wilsonify <tom.a.wilson@gmail.com>
2023-09-02 11:13:23 -0400

    valid terraform

--- p01-first-steps/c02-architectures/twentyfour-hour-video-python/infra/provider.tf
+++ p01-first-steps/c02-architectures/twentyfour-hour-video-python/infra/provider.tf
@@ -1,8 +1,12 @@
-provider "aws" {}
+provider "aws" {
+  region = "us-east-1"
+}
 
 terraform {
+  required_version = ">1.5.5"
   required_providers {
     aws = {
+      source  = "hashicorp/aws"
       version = "~> 3.76.1"
     }
   }
--- p01-first-steps/c02-architectures/twentyfour-hour-video-python/infra/s3_video_transcode.tf
+++ p01-first-steps/c02-architectures/twentyfour-hour-video-python/infra/s3_video_transcode.tf
@@ -1,25 +1,30 @@
 resource "aws_s3_bucket" "s3-serverless-video-transcode-python" {
-  arn                 = "arn:aws:s3:::064592191516-serverless-video-transcode-python"
   bucket              = "064592191516-serverless-video-transcode-python"
-  force_destroy       = "false"
+  force_destroy       = false
   hosted_zone_id      = "Z3AQBSTGFYJSTF"
-  object_lock_enabled = "false"
-  request_payer       = "BucketOwner"
+  object_lock_enabled = false
+}
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = "arn:aws:kms:us-east-1:064592191516:alias/aws/s3"
-        sse_algorithm     = "aws:kms"
-      }
+resource "aws_s3_bucket_request_payment_configuration" "s3-serverless-video-transcode-python-payer" {
+  bucket = aws_s3_bucket.s3-serverless-video-transcode-python.id
+  payer  = "BucketOwner"
+}
 
-      bucket_key_enabled = "true"
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3-serverless-video-transcode-python-sse" {
+  bucket = aws_s3_bucket.s3-serverless-video-transcode-python.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = "arn:aws:kms:us-east-1:064592191516:alias/aws/s3"
+      sse_algorithm     = "aws:kms"
     }
   }
+}
 
-  versioning {
-    enabled    = "false"
-    mfa_delete = "false"
+resource "aws_s3_bucket_versioning" "s3-serverless-video-transcode-python-versioning" {
+  bucket = aws_s3_bucket.s3-serverless-video-transcode-python.id
+  versioning_configuration {
+    status = "Disabled"
   }
 }
 
