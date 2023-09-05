171aceb9ebb47486eda18ebb204d83abc5d2267f
wilsonify <tom.a.wilson@gmail.com>
2023-09-05 07:54:35 -0400

    update path to mp4

--- p01-first-steps/c02-architectures/twentyfour-hour-video-sqs/test/upload.sh
+++ p01-first-steps/c02-architectures/twentyfour-hour-video-sqs/test/upload.sh
@@ -1,3 +1,3 @@
 source_abs_path="/home/thom/repos/serverless-architectures-aws/p01-first-steps/c02-architectures/twentyfour-hour-video/test/SampleVideo_1280x720_5mb.mp4"
-dest_uri="s3://064592191516-serverless-video-upload-python/uploads"
+dest_uri="s3://064592191516-serverless-video-upload-python/uploads/SampleVideo_1280x720_5mb.mp4"
 aws --profile 064592191516 s3 cp $source_abs_path $dest_uri
